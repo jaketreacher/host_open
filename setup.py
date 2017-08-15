992fa42e037f8c6c697831dc603e51fd131053df
Jake Treacher <git@jaketreacher.com>
2017-08-15 15:24:19 +0000

    Create basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='HostOpen',
+	  version='0.1',
+	  description='Open \'remote\' vagrant files locally',
+	  author='Jake Treacher',
+      author_email='git@jaketreacher.com',
+      url='https://github.com/jaketreacher/host_open',
+      packages=['host_open', ]
+      )
